e27fb1789d957a72f2b7be844e9edcb64638bbc6
ErikBoesen <me@erikboesen.com>
2019-09-12 12:01:57 -0400

    Remove and ignore Pods directory

--- Pods/Target Support Files/Pods-Yale Menus/Pods-Yale Menus-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yale_Menus : NSObject
-@end
-@implementation PodsDummy_Pods_Yale_Menus
-@end
--- Pods/Target Support Files/Pods-Yale MenusUITests/Pods-Yale MenusUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yale_MenusUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Yale_MenusUITests
-@end
