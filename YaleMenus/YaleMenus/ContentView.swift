cccba6974ff895244b9754400cad2b23a205f782
ErikBoesen <me@erikboesen.com>
2020-10-03 14:55:08 -0600

    Init new YaleMenus project

---
+++ YaleMenus/YaleMenus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YaleMenus
+//
+//  Created by Erik Kieran Boesen on 10/3/20.
+//  Copyright © 2020 Erik Boesen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
