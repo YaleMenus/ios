0bd6c7dcd7b2db26d82c595ec87953425106222b
ErikBoesen <me@erikboesen.com>
2020-10-03 14:42:54 -0600

    Use Alamofire

--- YaleDine/Package.swift
+++ YaleDine/Package.swift
@@ -12,14 +12,14 @@ let package = Package(
             targets: ["YaleDine"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "YaleDine",
-            dependencies: []),
+            dependencies: ["ReactiveMoya"]),
         .testTarget(
             name: "YaleDineTests",
             dependencies: ["YaleDine"]),
--- YaleDine/Sources/YaleDine/YaleDine.swift
+++ YaleDine/Sources/YaleDine/YaleDine.swift
@@ -1,38 +1,29 @@
+import Foundation
+import Alamofire
+
 struct YaleDine {
     static let API_ROOT = "https://yaledine.com/api/"
-        
-    static func get(endpoint: String, then: @escaping (Bool) -> Void) {
-        let url = URL(string: self.API_ROOT + endpoint)!
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
 
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print(error?.localizedDescription ?? "No data")
-                then(false)
-                return
-            }
-            let json = try? JSONSerialization.jsonObject(with: data, options: [])
-            if let json = json as? [String: Any] {
-                then(true)
-                print(json)
-            }
+    static func get(endpoint: String, type: TypeEnum, then: @escaping (Any) -> Void) {
+        AF.request(self.API_ROOT + endpoint, method: .get)
+        .responseJSON { response in
+            then(response)
         }
-        task.resume()
+    }
+
+    static func getLocations(then: @escaping ([Location]) -> Void) {
+        return self.get(endpoint: "locations", type: [Location], then: then)
     }
     
-    static func getLocations(then: @escaping (Bool) -> Void) {
-        return self.get("locations": <#T##String#>, then: <#T##(Bool) -> Void#>)
+    static func getLocation(id: Int, then: @escaping (Location?) -> Void) {
+        return self.get(endpoint: "locations/\(id)", type: Location, then: then)
     }
-    
+
 //    /locations
-//    /locations/:id
 //    /locations/:id
 //    /locations/:id/managers
 //    /locations/:id/meals
 //    /meals/:id
-//    /meals/:id/items
 //    /meals/:id/items
 //    /meals/:id/courses
 //    /courses/:id
