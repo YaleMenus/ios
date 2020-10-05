063a85e377234b16e0f8bdd64a75673af2c7be7e
ErikBoesen <me@erikboesen.com>
2020-10-04 21:41:46 -0600

    Add API enum

--- YaleMenus/YaleMenus/API.swift
+++ YaleMenus/YaleMenus/API.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+enum API {
+    case locations
+    case location(id: Int)
+    case managers(locationId: Int)
+    case meals(locationId: Int)
+    case meal(mealId: Int)
+    case courses(mealId: Int)
+    case course(courseId: Int)
+    case items(mealId: Int)
+    case item(itemId: Int)
+    case nutrition(itemId: Int)
+}
