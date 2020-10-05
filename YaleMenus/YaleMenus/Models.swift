80dcbfe4ec59214afd8ffdc9cb7d69cf655eaf06
ErikBoesen <me@erikboesen.com>
2020-10-04 21:28:29 -0600

    Add Nutrition model

--- YaleMenus/YaleMenus/Models.swift
+++ YaleMenus/YaleMenus/Models.swift
@@ -190,3 +190,127 @@ extension Item: Decodable {
         courseId = try container.decode(Int.self, forKey: .courseId)
     }
 }
+
+struct Nutrition {
+    let id: Int
+    let portionSize: String
+    let calories: String
+
+    let totalFat: String
+    let saturatedFat: String
+    let transFat: String
+    let cholesterol: String
+    let sodium: String
+    let totalCarbohydrate: String
+    let dietaryFiber: String
+    let totalSugars: String
+    let protein: String
+    let vitaminD: String
+    let vitaminA: String
+    let vitaminC: String
+    let calcium: String
+    let iron: String
+    let potassium: String
+
+    let totalFatPDV: Int
+    let saturatedFatPDV: Int
+    let transFatPDV: Int
+    let cholesterolPDV: Int
+    let sodiumPDV: Int
+    let totalCarbohydratePDV: Int
+    let dietaryFiberPDV: Int
+    let totalSugarsPDV: Int
+    let proteinPDV: Int
+    let vitaminDPDV: Int
+    let vitaminAPDV: Int
+    let vitaminCPDV: Int
+    let calciumPDV: Int
+    let ironPDV: Int
+    let potassiumPDV: Int
+
+    let itemId: Int
+}
+
+extension Nutrition: Decodable {
+    enum NutritionCodingKeys: String, CodingKey {
+        case id
+        case portionSize = "portion_size"
+        case calories
+
+        case totalFat = "total_fat"
+        case saturatedFat = "saturated_fat"
+        case transFat = "trans_fat"
+        case cholesterol
+        case sodium
+        case totalCarbohydrate = "total_carbohydrate"
+        case dietaryFiber = "dietary_fiber"
+        case totalSugars = "total_sugars"
+        case protein
+        case vitaminD = "vitamin_d"
+        case vitaminA = "vitamin_a"
+        case vitaminC = "vitamin_c"
+        case calcium
+        case iron
+        case potassium
+
+        case totalFatPDV = "total_fat_pdv"
+        case saturatedFatPDV = "saturated_fat_pdv"
+        case transFatPDV = "trans_fat_pdv"
+        case cholesterolPDV = "cholesterol_pdv"
+        case sodiumPDV = "sodium_pdv"
+        case totalCarbohydratePDV = "total_carbohydrate_pdv"
+        case dietaryFiberPDV = "dietary_fiber_pdv"
+        case totalSugarsPDV = "total_sugars_pdv"
+        case proteinPDV = "protein_pdv"
+        case vitaminDPDV = "vitamin_d_pdv"
+        case vitaminAPDV = "vitamin_a_pdv"
+        case vitaminCPDV = "vitamin_c_pdv"
+        case calciumPDV = "calcium_pdv"
+        case ironPDV = "iron_pdv"
+        case potassiumPDV = "potassium_pdv"
+
+        case itemId = "item_id"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: NutritionCodingKeys.self)
+
+        id = try container.decode(Int.self, forKey: .id)
+        portionSize = try container.decode(String.self, forKey: .portionSize)
+        calories = try container.decode(String.self, forKey: .calories)
+
+        totalFat = try container.decode(String.self, forKey: .totalFat)
+        saturatedFat = try container.decode(String.self, forKey: .saturatedFat)
+        transFat = try container.decode(String.self, forKey: .transFat)
+        cholesterol = try container.decode(String.self, forKey: .cholesterol)
+        sodium = try container.decode(String.self, forKey: .sodium)
+        totalCarbohydrate = try container.decode(String.self, forKey: .totalCarbohydrate)
+        dietaryFiber = try container.decode(String.self, forKey: .dietaryFiber)
+        totalSugars = try container.decode(String.self, forKey: .totalSugars)
+        protein = try container.decode(String.self, forKey: .protein)
+        vitaminD = try container.decode(String.self, forKey: .vitaminD)
+        vitaminA = try container.decode(String.self, forKey: .vitaminA)
+        vitaminC = try container.decode(String.self, forKey: .vitaminC)
+        calcium = try container.decode(String.self, forKey: .calcium)
+        iron = try container.decode(String.self, forKey: .iron)
+        potassium = try container.decode(String.self, forKey: .potassium)
+
+        totalFatPDV = try container.decode(Int.self, forKey: .totalFatPDV)
+        saturatedFatPDV = try container.decode(Int.self, forKey: .saturatedFatPDV)
+        transFatPDV = try container.decode(Int.self, forKey: .transFatPDV)
+        cholesterolPDV = try container.decode(Int.self, forKey: .cholesterolPDV)
+        sodiumPDV = try container.decode(Int.self, forKey: .sodiumPDV)
+        totalCarbohydratePDV = try container.decode(Int.self, forKey: .totalCarbohydratePDV)
+        dietaryFiberPDV = try container.decode(Int.self, forKey: .dietaryFiberPDV)
+        totalSugarsPDV = try container.decode(Int.self, forKey: .totalSugarsPDV)
+        proteinPDV = try container.decode(Int.self, forKey: .proteinPDV)
+        vitaminDPDV = try container.decode(Int.self, forKey: .vitaminDPDV)
+        vitaminAPDV = try container.decode(Int.self, forKey: .vitaminAPDV)
+        vitaminCPDV = try container.decode(Int.self, forKey: .vitaminCPDV)
+        calciumPDV = try container.decode(Int.self, forKey: .calciumPDV)
+        ironPDV = try container.decode(Int.self, forKey: .ironPDV)
+        potassiumPDV = try container.decode(Int.self, forKey: .potassiumPDV)
+
+        itemId = try container.decode(Int.self, forKey: .itemId)
+    }
+}
