e2b0fcce4e0bcf091748d274669d028385c21bd0
ErikBoesen <me@erikboesen.com>
2020-10-11 20:56:48 -0600

    Add definitions for functions in Networkable

--- YaleMenus/YaleMenus/NetworkManager.swift
+++ YaleMenus/YaleMenus/NetworkManager.swift
@@ -1,6 +1,21 @@
 import Foundation
 import Moya
 
+protocol Networkable {
+    var provider: MoyaProvider<API> { get }
+
+    func getLocations(completion: @escaping ([Location]) -> ());
+    func getLocation(id: Int, completion: @escaping (Location) -> ());
+    func getManagers(locationId: Int, completion: @escaping ([Manager]) -> ());
+    func getMeals(locationId: Int, date: String, completion: @escaping ([Meal]) -> ());
+    func getMeal(mealId: Int, completion: @escaping (Meal) -> ());
+    func getCourses(mealId: Int, completion: @escaping ([Course]) -> ());
+    func getCourse(courseId: Int, completion: @escaping (Course) -> ());
+    func getItems(mealId: Int, completion: @escaping ([Item]) -> ());
+    func getItem(itemId: Int, completion: @escaping (Item) -> ());
+    func getNutrition(itemId: Int, completion: @escaping (Nutrition) -> ());
+}
+
 struct NetworkManager {
     fileprivate let provider = MoyaProvider<API>(plugins: [NetworkLoggerPlugin()])
 }
