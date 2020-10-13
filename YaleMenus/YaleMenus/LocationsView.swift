2fd92acabf8a0db7e24cbf755e0d744630886ee8
ErikBoesen <me@erikboesen.com>
2020-10-12 18:20:41 -0600

    Add updated View for locations

--- YaleMenus/YaleMenus/LocationsView.swift
+++ YaleMenus/YaleMenus/LocationsView.swift
@@ -2,35 +2,14 @@ import SwiftUI
 import Foundation
 
 struct LocationsView : View {
-    @State var requested: Bool = false
+    @State var model = LocationsViewModel();
     
     var body: some View {
-        VStack {
-            Spacer()
-            VStack {
-                if self.requested {
-                    Button(action: {
-                        self.requested = false
-                    }) {
-                        ButtonContent(text: "Button pressed", color: .green)
-                    }
-                } else {
-                    Button(action: {
-                        self.requested = true
-                        API.requestAttention(then: { success in
-                            self.requested = success
-                            if success {
-                                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
-                                    self.requested = false
-                                })
-                            }
-                        })
-                    }) {
-                        ButtonContent(text: "Press this button")
-                    }
-                }
+        List(model.locations, selection: model.openLocation) { location in
+            VStack(alignment: .leading) {
+                Text(location.name)
+                Text(location.address)
             }
-            Spacer()
         }
     }
 }
