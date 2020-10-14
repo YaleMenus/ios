2e19c360f4f26ff4e7b8c1b72ea85c4040c51502
ErikBoesen <me@erikboesen.com>
2020-10-13 22:30:46 -0600

    Improve positioning and sizing of images on homepage

--- YaleMenus/YaleMenus/LocationsView.swift
+++ YaleMenus/YaleMenus/LocationsView.swift
@@ -21,14 +21,16 @@ struct GridStack<Content: View>: View {
     }
 
     var body: some View {
-        VStack {
-            ForEach(0 ..< self.rows, id: \.self) { row in
-                HStack {
-                    ForEach(0 ..< self.columns, id: \.self) { column in
-                        self.content(self.item(n: row * self.rows + column), row, column)
-                    }
+        GeometryReader { geometry in
+            VStack {
+                ForEach(0 ..< self.rows, id: \.self) { row in
+                    HStack {
+                        ForEach(0 ..< self.columns, id: \.self) { column in
+                            self.content(self.item(n: row * self.rows + column), row, column)
+                        }
+                    }.frame(width: geometry.size.width)
                 }
-            }
+            }.frame(height: geometry.size.height)
         }
     }
 }
@@ -41,8 +43,15 @@ struct LocationsView : View {
             if self.model.locations != nil {
                 GridStack(items: self.model.locations!, rows: 4, columns: 3) { location, row, col in
                     if (location != nil) {
+                        Image(String(location!.capacity))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .overlay(
+                                Image(location!.code)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                             )
                         Text(location!.name)
-                        Text(location!.address)
                     } else {
                         Text("Location goes here")
                     }
