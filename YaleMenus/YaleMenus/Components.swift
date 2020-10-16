3f34e236016366957d792737fad2870f9f905c24
ErikBoesen <me@erikboesen.com>
2020-10-15 18:30:45 -0600

    Remove ButtonContent component

--- YaleMenus/YaleMenus/Components.swift
+++ YaleMenus/YaleMenus/Components.swift
@@ -1,16 +1,2 @@
 import SwiftUI
 
-struct ButtonContent : View {
-    @State var text: String
-    @State var color: Color = .blue
-    
-    var body: some View {
-        Text(self.text)
-            .font(.title)
-            .multilineTextAlignment(.center)
-            .foregroundColor(.white)
-            .frame(width: 340, height: 100)
-            .background(self.color)
-            .cornerRadius(50)
-    }
-}
