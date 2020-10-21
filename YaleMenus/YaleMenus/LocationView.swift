244dbf72b8960b3239fd969fa9184151479a5ee3
ErikBoesen <me@erikboesen.com>
2020-10-20 18:01:15 -0600

    Add showing of meals

--- YaleMenus/YaleMenus/LocationView.swift
+++ YaleMenus/YaleMenus/LocationView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct LocationView : View {
     @ObservedObject var model: LocationViewModel
+    @State private var mealIndex = 0
 
     init(locationId: Int) {
         self.model = LocationViewModel(locationId: locationId)
@@ -10,8 +11,15 @@ struct LocationView : View {
 
     var body: some View {
         VStack {
-            if (self.model.location != nil) {
-                Text(self.model.location!.name)
+//            if (self.model.location != nil) {
+//                Text(self.model.location!.name)
+//            }
+            if (self.model.meals != nil) {
+                Picker(selection: $mealIndex, label: Text("Choose a meal?")) {
+                    ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
+                        Text(meal.name).tag(index)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
             } else {
                 LoaderView()
             }
--- YaleMenus/YaleMenus/LocationViewModel.swift
+++ YaleMenus/YaleMenus/LocationViewModel.swift
@@ -4,14 +4,28 @@ class LocationViewModel: ObservableObject, Identifiable {
     let id = UUID()
     let nm = NetworkManager()
     
+    var locationId: Int? = nil
     @Published var location: Location? = nil
+    @Published var date: Date = Date()
+    let formatter = DateFormatter()
+    @Published var meals: [Meal]? = nil
     
     init(locationId: Int) {
         nm.getLocation(id: locationId, completion: { location in
-            self.location = location;
+            self.location = location
         });
-//        nm.getMeals(location.id, completion: { meals in
-//            self.meals = meals;
-//        });
+        // TODO: we shouldn't need to store this, it should be accessible through self.location.
+        self.locationId = locationId
+        self.formatter.dateFormat = "yyyy-MM-dd"
+        self.getMeals()
+    }
+    
+    func getMeals() {
+        // TODO: use self.location?.id
+        nm.getMeals(locationId: self.locationId!,
+                    date: self.formatter.string(from: self.date),
+                    completion: { meals in
+            self.meals = meals
+        })
     }
 }
