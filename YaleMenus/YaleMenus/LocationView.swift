4983284a291dde4d15d43d624aa83aa53f43e129
ErikBoesen <me@erikboesen.com>
2020-11-16 19:19:35 -0700

    Add onChange event when switching meals

--- YaleMenus/YaleMenus/LocationView.swift
+++ YaleMenus/YaleMenus/LocationView.swift
@@ -1,6 +1,17 @@
 import Foundation
 import SwiftUI
 
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        return Binding(
+            get: { self.wrappedValue },
+            set: { selection in
+                self.wrappedValue = selection
+                handler(selection)
+        })
+    }
+}
+
 struct LocationView : View {
     @ObservedObject var model: LocationViewModel
     @State private var mealIndex = 0
@@ -10,19 +21,24 @@ struct LocationView : View {
     }
 
     var body: some View {
-        VStack {
-//            if (self.model.location != nil) {
-//                Text(self.model.location!.name)
-//            }
-            if (self.model.meals != nil) {
-                Picker(selection: $mealIndex, label: Text("Choose a meal?")) {
-                    ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
-                        Text(meal.name).tag(index)
+        NavigationView {
+            VStack {
+                if (self.model.location != nil && self.model.meals != nil) {
+                    // TODO: .onChange is native in iOS 14+, switch once we can ensure that most users will be on 14
+                    Picker(selection: $mealIndex.onChange(onChange), label: Text("Choose a meal?")) {
+                        ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
+                            Text(meal.name).tag(index)
+                        }
                     }
-                }.pickerStyle(SegmentedPickerStyle())
-            } else {
-                LoaderView()
+                    .pickerStyle(SegmentedPickerStyle())
+                } else {
+                    LoaderView()
+                }
             }
         }
+    }
+    
+    func onChange(mealIndex: Int) {
+        self.model.getItems(mealIndex: mealIndex)
     }
 }
