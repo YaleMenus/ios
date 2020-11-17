b7de28364888814d92ce7ad29abe48a3fb32840e
ErikBoesen <me@erikboesen.com>
2020-11-16 19:19:57 -0700

    Start adding logic for getting meal items

--- YaleMenus/YaleMenus/LocationViewModel.swift
+++ YaleMenus/YaleMenus/LocationViewModel.swift
@@ -3,13 +3,14 @@ import Foundation
 class LocationViewModel: ObservableObject, Identifiable {
     let id = UUID()
     let nm = NetworkManager()
-    
+
     var locationId: Int? = nil
     @Published var location: Location? = nil
     @Published var date: Date = Date()
     let formatter = DateFormatter()
     @Published var meals: [Meal]? = nil
-    
+    @Published var items: [[Item]?]? = nil
+
     init(locationId: Int) {
         nm.getLocation(id: locationId, completion: { location in
             self.location = location
@@ -17,15 +18,28 @@ class LocationViewModel: ObservableObject, Identifiable {
         // TODO: we shouldn't need to store this, it should be accessible through self.location.
         self.locationId = locationId
         self.formatter.dateFormat = "yyyy-MM-dd"
+        // TODO: only for testing while we don't have updated data.
+        self.date = self.formatter.date(from: "2020-09-24")!
         self.getMeals()
     }
-    
+
     func getMeals() {
         // TODO: use self.location?.id
         nm.getMeals(locationId: self.locationId!,
                     date: self.formatter.string(from: self.date),
                     completion: { meals in
             self.meals = meals
+            self.items = [[Item]?](repeating: nil, count: meals.count)
+            print(self.meals)
         })
+    }
+
+    func getItems(mealIndex: Int) {
+        // TODO: ensure this won't be called before nm.getMeals completes above
+        if (self.meals != nil && self.items != nil && mealIndex < self.items!.count) {
+            if (self.items![mealIndex] != nil) {
+                
+            }
+        }
     }
 }
