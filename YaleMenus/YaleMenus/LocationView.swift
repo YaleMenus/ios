b12ba67fab467b80936fda24851209c9623a80a9
ErikBoesen <me@erikboesen.com>
2020-11-17 18:25:00 -0700

    Remove navigation view on location page

--- YaleMenus/YaleMenus/LocationView.swift
+++ YaleMenus/YaleMenus/LocationView.swift
@@ -30,11 +30,8 @@ struct ItemPreviewView : View {
             Spacer()
         }
         .padding()
-        .overlay(
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(Color.white, lineWidth: 1)
-        )
         .background(Color.init(red: 241 / 255, green: 244 / 255, blue: 247 / 255))
+        // TODO: rounded border
     }
 }
 
@@ -47,40 +44,32 @@ struct LocationView : View {
     }
 
     var body: some View {
-        NavigationView {
-            VStack(alignment: .leading) {
-                if (self.model.location != nil && self.model.meals != nil) {
-                    if (self.model.items != nil && self.model.items![self.mealIndex] != nil) {
-                        ScrollView {
-                            VStack {
-                                ForEach(self.model.items![self.mealIndex]!, id: \.id) { item in
-                                    ItemPreviewView(item: item)
-                                }
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "chevron.left")
+                Text(self.model.location?.name ?? "")
+            }
+            if (self.model.location != nil && self.model.meals != nil) {
+                if (self.model.items != nil && self.model.items![self.mealIndex] != nil) {
+                    ScrollView {
+                        VStack {
+                            ForEach(self.model.items![self.mealIndex]!, id: \.id) { item in
+                                ItemPreviewView(item: item)
                             }
                         }
-                    } else {
-                        LoaderView()
                     }
-                    // TODO: .onChange is native in iOS 14+, switch once we can ensure that most users will be on 14
-                    Picker(selection: $mealIndex.onChange(onChange), label: Text("Choose a meal?")) {
-                        ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
-                            Text(meal.name).tag(index)
-                        }
-                    }.pickerStyle(SegmentedPickerStyle())
                 } else {
                     LoaderView()
                 }
-            }
-            .padding()
-            .navigationBarTitle(self.model.location?.name ?? "")
-            .navigationBarItems(leading:
-                HStack {
-                    Image(systemName: "chevron.left")
-                    Button("Back") {
-                        print("Back tapped!")
+                // TODO: .onChange is native in iOS 14+, switch once we can ensure that most users will be on 14
+                Picker(selection: $mealIndex.onChange(onChange), label: Text("Choose a meal?")) {
+                    ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
+                        Text(meal.name).tag(index)
                     }
-                }
-            )
+                }.pickerStyle(SegmentedPickerStyle())
+            } else {
+                LoaderView()
+            }
         }
     }
     
