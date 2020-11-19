e0d10ad627dc5fe329e17379a50741fd4399ea82
ErikBoesen <me@erikboesen.com>
2020-11-18 17:07:49 -0700

    Fix mispositioning of meal selector

--- YaleMenus/YaleMenus/AppDelegate.swift
+++ YaleMenus/YaleMenus/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        FontBlaster.blast() { (fonts) in
+//            print(fonts) // fonts is an array of Strings containing font names
+//        }
         return true
     }
 
--- YaleMenus/YaleMenus/LocationView.swift
+++ YaleMenus/YaleMenus/LocationView.swift
@@ -51,6 +51,12 @@ struct LocationView : View {
                 HeaderView(text: self.model.location?.name ?? "")
             }
             if (self.model.location != nil && self.model.meals != nil) {
+                // TODO: .onChange is native in iOS 14+, switch once we can ensure that most users will be on 14
+                Picker(selection: $mealIndex.onChange(onChange), label: Text("Choose a meal?")) {
+                    ForEach(Array(zip(self.model.meals!.indices, self.model.meals!)), id: \.0) { index, meal in
+                        Text(meal.name).tag(index)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
                 if (self.model.items != nil && self.model.items![self.mealIndex] != nil) {
                     ScrollView {
                         VStack {
