c424fc80ebb20e10522ad9af411fbdf035154ea5
ErikBoesen <me@erikboesen.com>
2020-11-25 18:06:30 -0700

    Add view for allergen

--- YaleMenus/YaleMenus/ItemView.swift
+++ YaleMenus/YaleMenus/ItemView.swift
@@ -1,6 +1,26 @@
 import Foundation
 import SwiftUI
 
+struct AllergenView : View {
+    let allergen: String
+
+    init(allergen: String) {
+        self.allergen = allergen
+    }
+    
+    var body: some View {
+         HStack {
+           Image("entree")
+               .resizable()
+               .aspectRatio(contentMode: .fit)
+               .frame(width: 60, alignment: .leading)
+           Spacer()
+           Text(self.allergen)
+           Spacer()
+       }
+    }
+}
+
 struct ItemView : View {
     @ObservedObject var model: ItemViewModel
 
