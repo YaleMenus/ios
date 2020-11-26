154249a68245bb05702d001163f7a090aba643f3
ErikBoesen <me@erikboesen.com>
2020-11-25 19:45:09 -0700

    Add nutrition facts listing

--- YaleMenus/YaleMenus/ItemView.swift
+++ YaleMenus/YaleMenus/ItemView.swift
@@ -24,6 +24,29 @@ struct AllergenView : View {
     }
 }
 
+struct NutritionRowView : View {
+    let label: String
+    let amount: String
+    let pdv: Int?
+    let offset: Int
+
+    init(label: String, amount: String, pdv: Int?, offset: Int = 0) {
+        self.label = label
+        self.amount = amount
+        self.pdv = pdv
+        self.offset = offset
+    }
+
+    var body: some View {
+        HStack {
+            Text(self.label)
+            Text(self.amount)
+            Spacer()
+            if (self.pdv != nil) { Text("\(self.pdv!)%") }
+        }
+    }
+}
+
 struct ItemView : View {
     @ObservedObject var model: ItemViewModel
 
@@ -54,6 +77,36 @@ struct ItemView : View {
 //                    if (self.model.item!.gluten) { AllergenView(allergen: "gluten") }
 //                    if (self.model.item!.coconut) { AllergenView(allergen: "coconut") }
                     Text("Ingredients: " + self.model.item!.ingredients)
+                }
+                if (self.model.nutrition != nil) {
+                    Text("Nutrition Facts")
+                    HStack {
+                        Text("Serving Size")
+                        Spacer()
+                        Text(self.model.nutrition!.portionSize)
+                    }
+                    HStack {
+                        Text("Calories")
+                        Spacer()
+                        Text(self.model.nutrition!.calories)
+                    }
+                    NutritionRowView(label: "Total Fat", amount: self.model.nutrition!.totalFat, pdv: self.model.nutrition!.totalFatPDV)
+                    NutritionRowView(label: "Saturated Fat", amount: self.model.nutrition!.saturatedFat, pdv: self.model.nutrition!.saturatedFatPDV)
+                    NutritionRowView(label: "Trans Fat", amount: self.model.nutrition!.transFat, pdv: self.model.nutrition!.transFatPDV)
+                    NutritionRowView(label: "Cholesterol", amount: self.model.nutrition!.cholesterol, pdv: self.model.nutrition!.cholesterolPDV)
+                    NutritionRowView(label: "Sodium", amount: self.model.nutrition!.sodium, pdv: self.model.nutrition!.sodiumPDV)
+                    NutritionRowView(label: "Total Carbohydrate", amount: self.model.nutrition!.totalCarbohydrate, pdv: self.model.nutrition!.totalCarbohydratePDV)
+                    NutritionRowView(label: "Dietary Fiber", amount: self.model.nutrition!.dietaryFiber, pdv: self.model.nutrition!.dietaryFiberPDV)
+//                    NutritionRowView(label: "Total Sugars", amount: self.model.nutrition!.totalSugars, pdv: self.model.nutrition!.totalSugarsPDV)
+//                    NutritionRowView(label: "Protein", amount: self.model.nutrition!.protein, pdv: self.model.nutrition!.proteinPDV)
+//                    NutritionRowView(label: "Vitamin D", amount: self.model.nutrition!.vitaminD, pdv: self.model.nutrition!.vitaminDPDV)
+//                    NutritionRowView(label: "Vitamin A", amount: self.model.nutrition!.vitamonA, pdv: self.model.nutrition!.vitamonAPDV)
+//                    NutritionRowView(label: "Vitamin C", amount: self.model.nutrition!.vitamonC, pdv: self.model.nutrition!.vitamonCPDV)
+//                    NutritionRowView(label: "Calcium", amount: self.model.nutrition!.calcium, pdv: self.model.nutrition!.calciumPDV)
+//                    NutritionRowView(label: "Iron", amount: self.model.nutrition!.iron, pdv: self.model.nutrition!.ironPDV)
+//                    NutritionRowView(label: "Potassium", amount: self.model.nutrition!.potassium, pdv: self.model.nutrition!.potassiumPDV)
+                } else {
+                    LoaderView()
                 }
             } else {
                 LoaderView()
