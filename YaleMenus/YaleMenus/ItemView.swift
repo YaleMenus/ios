f7013de5ec71f3dff63d2aedb10a7288f7b9d0b1
ErikBoesen <me@erikboesen.com>
2020-12-01 16:34:20 -0700

    Scroll only body of item page

--- YaleMenus/YaleMenus/ItemView.swift
+++ YaleMenus/YaleMenus/ItemView.swift
@@ -70,64 +70,59 @@ struct ItemView : View {
     }
 
     var body: some View {
-        VStack {
+        Group {
             HStack {
                 Image(systemName: "chevron.left")
                 Spacer()
                 HeaderView(text: self.model.item?.name ?? "")
             }
-            if (self.model.item != nil && self.model.nutrition != nil) {
-                VStack {
-                    if (self.model.item!.alcohol) { AllergenView(allergen: "alcohol") }
-                    if (self.model.item!.nuts) { AllergenView(allergen: "nuts") }
-                    if (self.model.item!.shellfish) { AllergenView(allergen: "shellfish") }
-                    if (self.model.item!.peanuts) { AllergenView(allergen: "peanuts") }
-                    if (self.model.item!.dairy) { AllergenView(allergen: "dairy") }
-                    if (self.model.item!.egg) { AllergenView(allergen: "egg") }
-                    if (self.model.item!.pork) { AllergenView(allergen: "pork") }
-                }
-                VStack {
-                    if (self.model.item!.fish) { AllergenView(allergen: "fish") }
-                    if (self.model.item!.soy) { AllergenView(allergen: "soy") }
-                    // TODO: why does uncommenting these break things??
-                    if (self.model.item!.wheat) { AllergenView(allergen: "wheat") }
-                    if (self.model.item!.gluten) { AllergenView(allergen: "gluten") }
-                    if (self.model.item!.coconut) { AllergenView(allergen: "coconut") }
-                    Text("Ingredients: \(self.model.item!.ingredients)")
-                }
-                VStack {
-                    Text("Nutrition Facts")
-                    HStack {
-                        Text("Serving Size")
-                        Spacer()
-                        Text(self.model.nutrition!.portionSize)
+            ScrollView {
+                if (self.model.item != nil && self.model.nutrition != nil) {
+                    VStack {
+                        if (self.model.item!.alcohol) { AllergenView(allergen: "alcohol") }
+                        if (self.model.item!.nuts) { AllergenView(allergen: "nuts") }
+                        if (self.model.item!.shellfish) { AllergenView(allergen: "shellfish") }
+                        if (self.model.item!.peanuts) { AllergenView(allergen: "peanuts") }
+                        if (self.model.item!.dairy) { AllergenView(allergen: "dairy") }
+                        if (self.model.item!.egg) { AllergenView(allergen: "egg") }
+                        if (self.model.item!.pork) { AllergenView(allergen: "pork") }
                     }
-                    HStack {
-                        Text("Calories")
-                        Spacer()
-                        Text(self.model.nutrition!.calories)
+                    VStack {
+                        if (self.model.item!.fish) { AllergenView(allergen: "fish") }
+                        if (self.model.item!.soy) { AllergenView(allergen: "soy") }
+                        // TODO: why does uncommenting these break things??
+                        if (self.model.item!.wheat) { AllergenView(allergen: "wheat") }
+                        if (self.model.item!.gluten) { AllergenView(allergen: "gluten") }
+                        if (self.model.item!.coconut) { AllergenView(allergen: "coconut") }
                     }
-                    NutritionRowView(label: "Total Fat", amount: self.model.nutrition!.totalFat, pdv: self.model.nutrition!.totalFatPDV, style: .main)
-                    NutritionRowView(label: "Saturated Fat", amount: self.model.nutrition!.saturatedFat, pdv: self.model.nutrition!.saturatedFatPDV, style: .sub)
-                    NutritionRowView(label: "Trans Fat", amount: self.model.nutrition!.transFat, pdv: self.model.nutrition!.transFatPDV, style: .sub)
-                    NutritionRowView(label: "Cholesterol", amount: self.model.nutrition!.cholesterol, pdv: self.model.nutrition!.cholesterolPDV, style: .main)
-                    NutritionRowView(label: "Sodium", amount: self.model.nutrition!.sodium, pdv: self.model.nutrition!.sodiumPDV, style: .main)
-                    NutritionRowView(label: "Total Carbohydrate", amount: self.model.nutrition!.totalCarbohydrate, pdv: self.model.nutrition!.totalCarbohydratePDV, style: .main)
-                    NutritionRowView(label: "Dietary Fiber", amount: self.model.nutrition!.dietaryFiber, pdv: self.model.nutrition!.dietaryFiberPDV, style: .sub)
+                    VStack {
+                        Text("Nutrition Facts")
+                        NutritionRowView(label: "Serving Size", amount: self.model.nutrition!.portionSize, pdv: nil, style: .serving)
+                        NutritionRowView(label: "Calories", amount: self.model.nutrition!.calories, pdv: nil, style: .calories)
+                        
+                        NutritionRowView(label: "Total Fat", amount: self.model.nutrition!.totalFat, pdv: self.model.nutrition!.totalFatPDV, style: .main)
+                        NutritionRowView(label: "Saturated Fat", amount: self.model.nutrition!.saturatedFat, pdv: self.model.nutrition!.saturatedFatPDV, style: .sub)
+                        NutritionRowView(label: "Trans Fat", amount: self.model.nutrition!.transFat, pdv: self.model.nutrition!.transFatPDV, style: .sub)
+                        NutritionRowView(label: "Cholesterol", amount: self.model.nutrition!.cholesterol, pdv: self.model.nutrition!.cholesterolPDV, style: .main)
+                        NutritionRowView(label: "Sodium", amount: self.model.nutrition!.sodium, pdv: self.model.nutrition!.sodiumPDV, style: .main)
+                        NutritionRowView(label: "Total Carbohydrate", amount: self.model.nutrition!.totalCarbohydrate, pdv: self.model.nutrition!.totalCarbohydratePDV, style: .main)
+                        NutritionRowView(label: "Dietary Fiber", amount: self.model.nutrition!.dietaryFiber, pdv: self.model.nutrition!.dietaryFiberPDV, style: .sub)
+                    }
+                    VStack {
+                        NutritionRowView(label: "Total Sugars", amount: self.model.nutrition!.totalSugars, pdv: self.model.nutrition!.totalSugarsPDV, style: .sub)
+                        NutritionRowView(label: "Protein", amount: self.model.nutrition!.protein, pdv: self.model.nutrition!.proteinPDV, style: .plain)
+                        NutritionRowView(label: "Vitamin D", amount: self.model.nutrition!.vitaminD, pdv: self.model.nutrition!.vitaminDPDV, style: .plain)
+                        NutritionRowView(label: "Vitamin A", amount: self.model.nutrition!.vitaminA, pdv: self.model.nutrition!.vitaminAPDV, style: .plain)
+                        NutritionRowView(label: "Vitamin C", amount: self.model.nutrition!.vitaminC, pdv: self.model.nutrition!.vitaminCPDV, style: .plain)
+                        NutritionRowView(label: "Calcium", amount: self.model.nutrition!.calcium, pdv: self.model.nutrition!.calciumPDV, style: .plain)
+                        NutritionRowView(label: "Iron", amount: self.model.nutrition!.iron, pdv: self.model.nutrition!.ironPDV, style: .plain)
+                        NutritionRowView(label: "Potassium", amount: self.model.nutrition!.potassium, pdv: self.model.nutrition!.potassiumPDV, style: .plain)
+                        Text("Ingredients: \(self.model.item!.ingredients)")
+                    }
+                } else {
+                    LoaderView()
                 }
-                VStack {
-                    NutritionRowView(label: "Total Sugars", amount: self.model.nutrition!.totalSugars, pdv: self.model.nutrition!.totalSugarsPDV, style: .sub)
-                    NutritionRowView(label: "Protein", amount: self.model.nutrition!.protein, pdv: self.model.nutrition!.proteinPDV, style: .plain)
-                    NutritionRowView(label: "Vitamin D", amount: self.model.nutrition!.vitaminD, pdv: self.model.nutrition!.vitaminDPDV, style: .plain)
-                    NutritionRowView(label: "Vitamin A", amount: self.model.nutrition!.vitaminA, pdv: self.model.nutrition!.vitaminAPDV, style: .plain)
-                    NutritionRowView(label: "Vitamin C", amount: self.model.nutrition!.vitaminC, pdv: self.model.nutrition!.vitaminCPDV, style: .plain)
-                    NutritionRowView(label: "Calcium", amount: self.model.nutrition!.calcium, pdv: self.model.nutrition!.calciumPDV, style: .plain)
-                    NutritionRowView(label: "Iron", amount: self.model.nutrition!.iron, pdv: self.model.nutrition!.ironPDV, style: .plain)
-                    NutritionRowView(label: "Potassium", amount: self.model.nutrition!.potassium, pdv: self.model.nutrition!.potassiumPDV, style: .plain)
-                }
-            } else {
-                LoaderView()
             }
-        }.padding()
+        }
     }
 }
