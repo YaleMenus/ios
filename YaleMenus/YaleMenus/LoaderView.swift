4306dc2939854177b2044dd4ecb5b830d2f58903
ErikBoesen <me@erikboesen.com>
2020-12-29 12:46:19 -0500

    Speed up loading animation slightly

--- YaleMenus/YaleMenus/LoaderView.swift
+++ YaleMenus/YaleMenus/LoaderView.swift
@@ -5,7 +5,7 @@ struct LoaderView : View {
     @State private var isAnimating = false
 
     var animation: Animation {
-        Animation.linear(duration: 1.5)
+        Animation.linear(duration: 1)
         .repeatForever(autoreverses: false)
     }
 
