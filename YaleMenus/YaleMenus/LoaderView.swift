9bc4c43300aa2777044573684ccf857a7f20b702
ErikBoesen <me@erikboesen.com>
2020-12-28 23:55:49 -0500

    Add loader content

--- YaleMenus/YaleMenus/LoaderView.swift
+++ YaleMenus/YaleMenus/LoaderView.swift
@@ -2,9 +2,26 @@ import SwiftUI
 import Foundation
 
 struct LoaderView : View {
+    @State private var isAnimating = false
+
+    var animation: Animation {
+        Animation.linear(duration: 0.5)
+        .repeatForever(autoreverses: false)
+    }
+
     var body: some View {
-        // TODO: add more interesting content
-        Text("Loading...")
-            .frame(maxHeight: .infinity, alignment: .topLeading)
+        VStack {
+            GeometryReader { geometry in
+                Image("logo").resizable()
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: geometry.size.width / 2)
+                    .rotation3DEffect(Angle.degrees(self.isAnimating ? 360 : 0), axis: (x: 0, y: 1, z: 0))
+                    .animation(self.animation)
+                    .onAppear {
+                        self.isAnimating = true
+                    }
+            }
+        }.frame(maxHeight: .infinity, alignment: .topLeading)
     }
 }
