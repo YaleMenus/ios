d107789390c597fe07b7ff966c0a85495cee8e87
ErikBoesen <me@erikboesen.com>
2020-12-29 02:13:39 -0500

    Add proper checkboxes on Settings page

--- YaleMenus/YaleMenus/SettingsView.swift
+++ YaleMenus/YaleMenus/SettingsView.swift
@@ -2,15 +2,24 @@ import SwiftUI
 
 struct CheckboxView: View {
     var label: String
+    @State var checked: Bool = false
     
-    init(label: String) {
-        
+    init(label: String, checked: Bool = false) {
+        self.label = label
+        self.checked = checked
+    }
+    
+    func toggle() {
+        self.checked = !self.checked
     }
     
     var body: some View {
-        HStack {
-            
-        }
+        Button(action: toggle) {
+            HStack {
+                Image(systemName: self.checked ? "xmark.circle": "circle")
+                Text(self.label)
+            }.frame(maxWidth: .infinity, alignment: .topLeading)
+        }.buttonStyle(PlainButtonStyle())
     }
 }
 
@@ -19,16 +28,34 @@ struct SettingsView: View {
     
     var body: some View {
         VStack {
-        HeaderView(text: "Settings")
-        ScrollView {
-            VStack {
+            HeaderView(text: "Settings")
+            ScrollView {
                 Text("Dietary Restrictions")
                     .font(.title)
-                VStack {
-                    
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                Text("I cannot eat:")
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                Group {
+                    CheckboxView(label: "Meat (I'm Vegetarian)")
+                    CheckboxView(label: "Animal Products (I'm Vegan)")
+                    CheckboxView(label: "Alcohol")
+                    CheckboxView(label: "Nuts")
+                    CheckboxView(label: "Shellfish")
+                    CheckboxView(label: "Peanuts")
+                    CheckboxView(label: "Dairy")
                 }
-            }
-        // TODO: is this still needed?
-        }.frame(maxWidth: .infinity)
+                Group {
+                    CheckboxView(label: "Egg")
+                    CheckboxView(label: "Pork")
+                    CheckboxView(label: "Fish")
+                    CheckboxView(label: "Soy")
+                    CheckboxView(label: "Wheat")
+                    CheckboxView(label: "Gluten")
+                    CheckboxView(label: "Coconut")
+                }
+                CheckboxView(label: "Show Nutrition Facts")
+            // TODO: is this still needed?
+            }.frame(maxWidth: .infinity, alignment: .topLeading)
+        }.padding()
     }
 }
