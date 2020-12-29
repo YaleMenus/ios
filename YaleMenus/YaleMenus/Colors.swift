0aa6d2a255f1f03e1b99c6caf673e05bffc593b8
ErikBoesen <me@erikboesen.com>
2020-12-28 23:38:36 -0500

    Clean up colors file

--- YaleMenus/YaleMenus/Colors.swift
+++ YaleMenus/YaleMenus/Colors.swift
@@ -1,17 +1,7 @@
 import Foundation
 import SwiftUI
 
-struct ColorManager {
-    // create static variables for custom colors
-    static let foreground = Color("Foreground")
-
-    //... add the rest of your colors here
-}
-
-// Or you can use an extension
-// this will allow you to just type .spotifyGreen and you wont have to use ColorManager.spotifyGreen
 extension Color {
     static let foreground = Color("Foreground")
- 
-    // ... add the rest of your colors here
+    static let medium = Color("Medium")
 }
