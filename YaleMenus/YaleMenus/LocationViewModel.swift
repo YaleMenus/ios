e38c0a86c3555e0565c20ee76941f95aa95440a5
ErikBoesen <me@erikboesen.com>
2020-12-30 14:27:54 -0500

    Add logic to check if foods are blacklisted

--- YaleMenus/YaleMenus/LocationViewModel.swift
+++ YaleMenus/YaleMenus/LocationViewModel.swift
@@ -3,11 +3,13 @@ import Foundation
 class LocationViewModel: ObservableObject, Identifiable {
     let id = UUID()
     let nm = NetworkManager()
+    let settings = Settings()
 
     @Published var location: Location
     @Published var meals: [Date: [Meal]] = [:]
     @Published var mealNames: [String]? = nil
     @Published var items: [Date: [[Item]?]] = [:]
+    @Published var allowed: [Date: [[Bool]?]] = [:]
     @Published var date: Date = Date()
     public let formatterInternal = DateFormatter()
     public let formatterExternal = DateFormatter()
@@ -25,11 +27,13 @@ class LocationViewModel: ObservableObject, Identifiable {
         // TODO: use self.location?.id
         // TODO: may crash when spamming buttons
         let date = self.date
+        
         nm.getMeals(locationId: self.location.id,
                     date: self.formatterInternal.string(from: date),
                     completion: { meals in
             self.meals[date] = meals
             self.items[date] = [[Item]?](repeating: nil, count: meals.count)
+            self.allowed[date] = [[Bool]?](repeating: nil, count: meals.count)
             // TODO: switch to whatever current/soonest meal is
             self.getItems(date: date, mealIndex: 0)
         })
@@ -43,6 +47,25 @@ class LocationViewModel: ObservableObject, Identifiable {
             self.items[date]![mealIndex] == nil
         ) {
             nm.getItems(mealId: self.meals[date]![mealIndex].id, completion: { items in
+                self.allowed[date]![mealIndex] = items.map { item in
+                    !(
+                        (self.settings.vegetarian && !item.vegetarian) ||
+                        (self.settings.vegan && !item.vegan) ||
+                        (self.settings.alcohol && item.alcohol) ||
+                        (self.settings.nuts && item.nuts) ||
+                        (self.settings.shellfish && item.shellfish) ||
+                        (self.settings.peanuts && item.peanuts) ||
+                        (self.settings.dairy && item.dairy) ||
+                        (self.settings.egg && item.egg) ||
+                        (self.settings.pork && item.pork) ||
+                        (self.settings.fish && item.fish) ||
+                        (self.settings.soy && item.soy) ||
+                        (self.settings.wheat && item.wheat) ||
+                        (self.settings.gluten && item.gluten) ||
+                        (self.settings.coconut && item.coconut) ||
+                        (self.settings.alcohol && item.alcohol)
+                    )
+                }
                 self.items[date]![mealIndex] = items
             })
         }
