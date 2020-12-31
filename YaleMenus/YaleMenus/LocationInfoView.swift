edfd4635336cb6fec5d14b855c94dcaef29b4972
ErikBoesen <me@erikboesen.com>
2020-12-30 23:42:56 -0500

    Support links in info page

--- YaleMenus/YaleMenus/LocationInfoView.swift
+++ YaleMenus/YaleMenus/LocationInfoView.swift
@@ -4,19 +4,33 @@ import SwiftUI
 struct InfoRowView : View {
     var icon: String
     var text: String
+    var link: String?
 
-    init(icon: String, text: String) {
+    init(icon: String, text: String, link: String? = nil) {
         self.icon = icon
         self.text = text
+        self.link = link
     }
 
     var body: some View {
         HStack {
             Image(systemName: self.icon)
                 .foregroundColor(.foreground)
-            Text(self.text)
-                .font(.appBodyMedium)
-                .foregroundColor(.foreground)
+            if (self.link != nil) {
+                Button(action: {
+                    print(self.link)
+                    let url = URL(string: self.link!)!
+                    UIApplication.shared.open(url)
+                }) {
+                    Text(self.text)
+                        .font(.appBodyBold)
+                        .foregroundColor(.foreground)
+                }
+            } else {
+                Text(self.text)
+                    .font(.appBodyMedium)
+                    .foregroundColor(.foreground)
+            }
             Spacer()
         }
     }
@@ -39,7 +53,11 @@ struct LocationInfoView : View {
                         .foregroundColor(.foreground)
                     InfoRowView(icon: "house", text: self.model.location.address)
                     InfoRowView(icon: "location.fill", text: "(\(self.model.location.latitude), \(self.model.location.longitude))")
-                    InfoRowView(icon: "phone.fill", text: self.model.location.phone)
+                    InfoRowView(
+                        icon: "phone.fill",
+                        text: self.model.location.phone,
+                        link: "tel://" + self.model.location.phone.filter { "() -".range(of: String($0)) == nil }
+                    )
                 }
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.foreground)
