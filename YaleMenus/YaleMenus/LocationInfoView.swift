83cb2902be2c878bfd62f032d74ab17b89a30949
ErikBoesen <me@erikboesen.com>
2020-12-30 23:13:20 -0500

    Add InfoRowView

--- YaleMenus/YaleMenus/LocationInfoView.swift
+++ YaleMenus/YaleMenus/LocationInfoView.swift
@@ -1,6 +1,26 @@
 import Foundation
 import SwiftUI
 
+struct InfoRowView : View {
+    var icon: String
+    var text: String
+
+    init(icon: String, text: String) {
+        self.icon = icon
+        self.text = text
+    }
+
+    var body: some View {
+        HStack {
+            Image(systemName: self.icon)
+                .foregroundColor(.foreground)
+            Text(self.text)
+                .font(.appBodyMedium)
+                .foregroundColor(.foreground)
+        }
+    }
+}
+
 struct LocationInfoView : View {
     @ObservedObject var model: LocationInfoViewModel
 
