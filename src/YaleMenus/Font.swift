0e065b28a76563b29ac591fc6a4d6e0bb8e4aa88
ErikBoesen <me@erikboesen.com>
2021-01-11 22:23:52 -0500

    Make body font smaller

--- src/YaleMenus/Font.swift
+++ src/YaleMenus/Font.swift
@@ -5,7 +5,7 @@ extension Font {
     static let appHeader = Font.custom("Montserrat-Medium", size: 50)
     static let appTitle = Font.custom("Montserrat-SemiBold", size: 25)
     static let appTitleLight = Font.custom("Montserrat-Regular", size: 25)
-    static let appBody = Font.custom("Montserrat-Regular", size: 18)
-    static let appBodyMedium = Font.custom("Montserrat-Medium", size: 18)
+    static let appBody = Font.custom("Montserrat-Regular", size: 16)
+    static let appBodyMedium = Font.custom("Montserrat-Medium", size: 16)
     static let appBodyBold = Font.custom("Montserrat-ExtraBold", size: 18)
 }
